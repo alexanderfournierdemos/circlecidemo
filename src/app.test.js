1a01521bd914a362adefeddcd881f62183e5b118
afournier <alexander.fournier@experian.com>
2019-06-04 20:29:17 -0700

    reactdom

--- src/app.test.js
+++ src/app.test.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  React.render(<App />, document.getElementById('root'))
+  ReactDOM.render(<App />, document.getElementById('root'))
 });
